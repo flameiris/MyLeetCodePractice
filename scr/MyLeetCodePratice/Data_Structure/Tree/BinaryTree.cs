e4c32a26525a613d2cd7799d395dd3cd88ad53b1
flameiris <373168696@qq.com>
2020-10-21 17:29:48 +0800

    submit

--- scr/MyLeetCodePratice/Data_Structure/Tree/BinaryTree.cs
+++ scr/MyLeetCodePratice/Data_Structure/Tree/BinaryTree.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Xml;
 
@@ -47,6 +48,45 @@ namespace Tree
             return node1;
         }
 
+        /// <summary>
+        ///  手动构建二叉树
+        ///       1
+        ///    2     3
+        ///  4   5     6
+        /// </summary>
+        /// <param name="queue"></param>
+        /// <returns></returns>
+        public static BinaryTreeNode CreateBinaryTree(List<int?> list)
+        {
+            if (list[0] == null) return null;
+            Queue<BinaryTreeNode> queue = new Queue<BinaryTreeNode>();
+            BinaryTreeNode node = new BinaryTreeNode((int)list[0]);
+            queue.Enqueue(node);
+            var root = node;
+            var n = 1;
+            while (queue.Any())
+            {
+                root = queue.Dequeue();
+
+                if (list[n] != null)
+                {
+                    root.LeftNode = new BinaryTreeNode((int)list[n]);
+                    queue.Enqueue(root.LeftNode);
+                }
+                n++;
+                if (n == list.Count) break;
+
+                if (list[n] != null)
+                {
+                    root.RightNode = new BinaryTreeNode((int)list[n]);
+                    queue.Enqueue(root.RightNode);
+                }
+                n++;
+                if (n == list.Count) break;
+            }
+
+            return node;
+        }
 
 
 
@@ -58,10 +98,10 @@ namespace Tree
         /// <returns></returns>
         public static IList<int> LevelTraversal(BinaryTreeNode node)
         {
-            //1.将根节点插入队列
-            //2.从队列中取出节点，判断节点如果有子节点，就将子节点插入队列
-            //3.遍历过的节点出队列
-
+            //1.将根节点插入队列 
+            //2.从队列中取出根节点，判断根节点如果有子节点，就依次将子节点插入队列【访问根节点】
+            //3.从队列中取出左子节点，判断左子节点是否存在子节点，依次将左子节点的子节点插入队列。【访问左子树】
+            //4.从队列中取出右子节点，判断右子节点是否存在子节点，依次将右子节点的子节点插入队列。【访问右子树】
 
             IList<int> res = new List<int>();
             Queue<BinaryTreeNode> queue = new Queue<BinaryTreeNode>();
@@ -86,11 +126,11 @@ namespace Tree
 
 
         /// <summary>
-        /// 前序遍历【迭代】 使用【栈】辅助存储节点。
-        /// 1.先遍历根节点。
-        /// 2.再遍历左子节点一直遍历到左侧最深层的叶子节点。
+        /// 前序遍历（中左右）【迭代】    使用【栈】辅助存储节点。
+        /// 1.先访问根节点值。
+        /// 2.再遍历访问左子节点的值，一直遍历访问到左侧最深层的叶子节点值。
         /// 3.如果左侧最深层叶子节点存在兄弟节点，则将其视为根节点继续步骤1，2。
-        /// 4.遍历根节点的右子节点，将其视为根节点继续步骤1，2。
+        /// 4.遍历访问根节点的右子节点，将其视为根节点继续步骤1，2。
         /// 时间复杂度：O(n)  空间复杂度：O(n)。
         /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/144er-cha-shu-de-qian-xu-bian-li-by-chenqian/
         /// </summary>
@@ -98,6 +138,12 @@ namespace Tree
         /// <returns></returns>
         public static IList<int> PreOrderTraverseIteration(BinaryTreeNode node)
         {
+            //1.将根节点入栈
+            //2.从栈中取出根节点，将根节点的右子节点 和 左子节点入栈【访问根节点，利用栈先入后出，入栈子节点】
+            //3.从栈中取出左子节点，将左子节点的右子节点 和 左子节点的左子节点入栈【访问左子节点，入栈左子节点的子节点】
+            //4.从栈中取出右子节点，将右子节点的右子节点 和 右子节点的左子节点入栈【访问右子节点，入栈右子节点的子节点】
+
+
             IList<int> res = new List<int>();
             //利用栈临时存储迭代内容
             Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
@@ -120,11 +166,11 @@ namespace Tree
         }
 
         /// <summary>
-        /// 前序遍历【递归】 
-        /// 1.先遍历根节点。
-        /// 2.再遍历左子节点一直遍历到左侧最深层的叶子节点。
+        /// 前序遍历（中左右）【递归】 
+        /// 1.先访问根节点值。
+        /// 2.再遍历访问左子节点的值，一直遍历访问到左侧最深层的叶子节点值。
         /// 3.如果左侧最深层叶子节点存在兄弟节点，则将其视为根节点继续步骤1，2。
-        /// 4.遍历根节点的右子节点，将其视为根节点继续步骤1，2。
+        /// 4.遍历访问根节点的右子节点，将其视为根节点继续步骤1，2。
         /// 时间复杂度：O(n)  空间复杂度：O(n)。
         /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/144er-cha-shu-de-qian-xu-bian-li-by-chenqian/
         /// </summary>
@@ -149,10 +195,11 @@ namespace Tree
 
 
         /// <summary>
-        /// 中序遍历【迭代】 使用【栈】辅助存储节点
-        /// 1.从根节点开始，遍历到树的最左节点
-        /// 2.访问过最左节点后，如该最左节点存在右子节点，则需要访问该右子节点（将最左节点看作子树的根节点，右子节点就是它的右子树，因此按照遍历规则需要对右子树进行中序遍历）
+        /// 中序遍历（左中右）【迭代】 使用【栈】辅助存储节点
+        /// 1.从根节点开始，遍历到树的最左节点，访问最左节点的值。
+        /// 2.如该最左节点存在右子节点，则需要遍历该右子节点，重复步骤1，2（将最左节点看作子树的根节点，右子节点就是它的右子树，因此按照遍历规则需要对右子树进行中序遍历）
         /// 3.继续访问最左节点的根节点
+        /// 4.继续访问最左节点的右侧兄弟节点，如该兄弟节点存在子节点，则需要遍历该兄弟节点，重复步骤。
         /// 时间复杂度：O(n)  空间复杂度：O(n)
         /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/144er-cha-shu-de-qian-xu-bian-li-by-chenqian/
         /// </summary>
@@ -171,6 +218,7 @@ namespace Tree
             var cur = node;
             while (stack.Any() || cur != null)
             {
+                // 使用【栈】辅助存储根节点到最深左子节点之间的所有节点
                 while (cur != null)
                 {
                     stack.Push(cur);
@@ -178,6 +226,8 @@ namespace Tree
                 }
                 node = stack.Pop();
                 res.Add(node.Data);
+
+                //如该最左节点存在右子节点，则需要遍历该右子节点，将右子节点视作根节点
                 if (node.RightNode != null)
                 {
                     cur = node.RightNode;
@@ -188,10 +238,11 @@ namespace Tree
         }
 
         /// <summary>
-        /// 中序遍历【递归】
-        /// 1.从根节点开始，遍历到树的最左节点
-        /// 2.访问过最左节点后，如该最左节点存在右子节点，则需要访问该右子节点（将最左节点看作子树的根节点，右子节点就是它的右子树，因此按照遍历规则需要对右子树进行中序遍历）
+        /// 中序遍历（左中右）【递归】
+        /// 1.从根节点开始，遍历到树的最左节点，访问最左节点的值。
+        /// 2.如该最左节点存在右子节点，则需要遍历该右子节点，重复步骤1，2（将最左节点看作子树的根节点，右子节点就是它的右子树，因此按照遍历规则需要对右子树进行中序遍历）
         /// 3.继续访问最左节点的根节点
+        /// 4.继续访问最左节点的右侧兄弟节点，如该兄弟节点存在子节点，则需要遍历该兄弟节点，重复步骤。
         /// 时间复杂度：O(n)    空间复杂度：O(n)  
         /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/144er-cha-shu-de-qian-xu-bian-li-by-chenqian/
         /// </summary>
@@ -216,7 +267,12 @@ namespace Tree
 
 
         /// <summary>
-        /// 后序遍历【迭代】
+        /// 后序遍历（左右中）【迭代】  使用【栈】辅助存储节点
+        /// 1.从根节点开始，遍历到树的最左节点，访问最左节点值。
+        /// 2.如该最左节点存在右子节点，则需要遍历该右子节点（将最左节点看作子树的根节点，右子节点就是它的右子树，因此按照遍历规则需要对右子树进行后序遍历）
+        /// 3.继续遍历最左节点右侧的兄弟节点，如果兄弟节点存在子节点，则需要遍历兄弟节点的子节点，重复步骤。
+        /// 4.继续访问兄弟节点值。
+        /// 5.继续访问最左节点的父节点的值。
         /// 时间复杂度：O(n)  空间复杂度：O(n)
         /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/144er-cha-shu-de-qian-xu-bian-li-by-chenqian/
         /// </summary>
@@ -254,6 +310,11 @@ namespace Tree
 
         /// <summary>
         /// 后序遍历【递归】
+        /// 1.从根节点开始，遍历到树的最左节点，访问最左节点值。
+        /// 2.如该最左节点存在右子节点，则需要遍历该右子节点（将最左节点看作子树的根节点，右子节点就是它的右子树，因此按照遍历规则需要对右子树进行后序遍历）
+        /// 3.继续遍历最左节点右侧的兄弟节点，如果兄弟节点存在子节点，则需要遍历兄弟节点的子节点，重复步骤。
+        /// 4.继续访问兄弟节点值。
+        /// 5.继续访问最左节点的父节点的值。
         /// 时间复杂度：O(n)  空间复杂度：O(n)
         /// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/solution/144er-cha-shu-de-qian-xu-bian-li-by-chenqian/
         /// </summary>
@@ -275,7 +336,110 @@ namespace Tree
             }
         }
 
+        /// <summary>
+        /// 后序遍历（左右中）【迭代】  调整前序遍历代码，并且反转结果
+        /// 前序遍历是 中左右，后序遍历是左右中。 
+        /// 反转入栈子节点顺序得 中右左，再反转结果得 左右中。
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static IEnumerable<int> PostOrderTraverseByReversePre(BinaryTreeNode node)
+        {
+
+            IList<int> res = new List<int>();
+            //利用栈临时存储迭代内容
+            Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
+            stack.Push(node);
+
+            while (stack.Any())
+            {
+                //从栈中取出 当前迭代内容
+                node = stack.Pop();
+                if (node != null)
+                {
+                    res.Add(node.Data);
+
+                    //入栈 子节点，前序遍历是入栈右子节点，左子节点。 这里需要入栈左子节点，右子节点
+                    stack.Push(node.LeftNode);
+                    stack.Push(node.RightNode);
+                }
+            }
+            //反转结果
+            return res.Reverse();
+        }
 
 
+
+
+        /// <summary>
+        /// 前序遍历、中序遍历、后序遍历 统一写法
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static IList<int> UnitedTraverse(BinaryTreeNode node, string type)
+        {
+            IList<int> res = new List<int>();
+            Stack<BinaryTreeNode> stack = new Stack<BinaryTreeNode>();
+
+            if (node != null) stack.Push(node);
+
+            while (stack.Any())
+            {
+                node = stack.Pop();
+                if (node != null)
+                {
+
+                    switch (type)
+                    {
+                        case "前序遍历":
+                            if (node.RightNode != null)
+                                stack.Push(node.RightNode);
+
+                            if (node.LeftNode != null)
+                                stack.Push(node.LeftNode);
+
+                            stack.Push(node);
+                            stack.Push(null);
+                            break;
+
+                        case "中序遍历":
+                            if (node.RightNode != null)
+                                stack.Push(node.RightNode);
+
+                            stack.Push(node);
+                            stack.Push(null);
+
+                            if (node.LeftNode != null)
+                                stack.Push(node.LeftNode);
+                            break;
+
+                        case "后序遍历":
+                            stack.Push(node);
+                            stack.Push(null);
+
+                            if (node.RightNode != null)
+                                stack.Push(node.RightNode);
+
+                            if (node.LeftNode != null)
+                                stack.Push(node.LeftNode);
+
+                            break;
+                        default:
+                            break;
+                    }
+
+
+
+                }
+                else
+                {
+                    node = stack.Pop();
+                    res.Add(node.Data);
+
+                }
+
+            }
+            return res;
+        }
     }
 }
--- scr/MyLeetCodePratice/Data_Structure/Tree/Program.cs
+++ scr/MyLeetCodePratice/Data_Structure/Tree/Program.cs
@@ -9,13 +9,21 @@ namespace Tree
     {
         static void Main(string[] args)
         {
-
+            #region 构建、前序遍历、中序遍历、后序遍历
 
             ///  手动构建二叉树
             ///       1
             ///    2     3
             ///  4   5     6
-            var binaryTree = BinaryTreeNode.CreateBinaryTree();
+            //var binaryTree = BinaryTreeNode.CreateBinaryTree();
+
+            ///  自动构建二叉树
+            ///       1
+            ///    2     3
+            ///  4     6
+            ///8  
+            List<int?> list = new List<int?> { 1, 2, 3, 4, null, 6, null, 8 };
+            var binaryTree = BinaryTreeNode.CreateBinaryTree(list);
             Console.WriteLine($"层序遍历(迭代)={{ {string.Join(",", BinaryTreeNode.LevelTraversal(binaryTree).ToList())}  }}");
 
 
@@ -30,6 +38,39 @@ namespace Tree
             ///  后序遍历
             Console.WriteLine($"后序遍历(迭代)={{ {string.Join(",", BinaryTreeNode.PostOrderTraverseIteration(binaryTree).ToList())}  }}");
             Console.WriteLine($"后序遍历(递归)={{ {string.Join(",", BinaryTreeNode.PostOrderTraverseRecursion(binaryTree).ToList())}  }}");
+            Console.WriteLine($"后序遍历-反转前序遍历(迭代)={{ {string.Join(",", BinaryTreeNode.PostOrderTraverseByReversePre(binaryTree).ToList())}  }}");
+
+            ///  前序遍历、中序遍历、后序遍历 统一写法
+            Console.WriteLine($"前序遍历-统一写法(迭代)={{ {string.Join(",", BinaryTreeNode.UnitedTraverse(binaryTree, "前序遍历").ToList())}  }}");
+            Console.WriteLine($"中序遍历-统一写法(迭代)={{ {string.Join(",", BinaryTreeNode.UnitedTraverse(binaryTree, "中序遍历").ToList())}  }}");
+            Console.WriteLine($"后序遍历-统一写法(迭代)={{ {string.Join(",", BinaryTreeNode.UnitedTraverse(binaryTree, "后序遍历").ToList())}  }}");
+
+
+
+
+            #endregion
+
+
+
+            #region 力扣
+
+            //二叉树的最小深度
+            var tree = BinaryTreeNode.CreateBinaryTree(new List<int?> { 3, 9, 20, null, null, 15, 7 });
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
 
 
             Console.Read();
